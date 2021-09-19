fb5743e89b82a013c60881a9aa9c5e648f6747fd
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-19 22:36:43 +0530

    bonus exercise: additional js function added

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
+const nextButton = document.querySelector("#next-button");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const changeBlock = document.querySelector("#change-block");
+const cashBlock = document.querySelector("#cash-block");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validateBillAndCashAmount()   {
+nextButton.addEventListener("click", function validateBillAmount()  {
+    message.style.display = "none";
+    if(billAmount.value != "" ) {
+        if(billAmount.value > 0 )    {
+            cashBlock.style.display = "flex";
+        } else  {
+            showMessage("Invalid Bill Amount");
+        }
+    }   else    {
+        showMessage("Please Enter the Bill Amount");
+    }
+});
+checkButton.addEventListener("click", function validateCashAmount()   {
     message.style.display = "none";
     if(billAmount.value > 0 )    {
         if (cashGiven.value >= billAmount.value)    {
             const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+            if(amountToBeReturned === 0)    {
+                showMessage("Bill Amount is equal to cash amount, No change required.");
+            }   else{
+                calculateChange(amountToBeReturned);
+            }
         } else  {
             showMessage("The cash provided should atleast be equal to the bill amount");
         }
@@ -26,6 +45,7 @@ function calculateChange(amountToBeReturned)  {
         amountToBeReturned %= availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
+    changeBlock.style.display = "flex";
 }
 function showMessage(innermessage)  {
     message.style.display = "block";
